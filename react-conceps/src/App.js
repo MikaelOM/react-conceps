92212fdcbc777a7a9fddbb925b5fdf8fcc1998ae
Mikael Henrique <el.mh506@gmail.com>
2022-06-14 22:59:19 -0300

    init concep useEffect

--- react-conceps/src/App.js
+++ react-conceps/src/App.js
@@ -1,9 +1,18 @@
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [userName, setUserName] = useState()
+
+  useEffect(() => {
+    console.log(userName)
+  }, [userName])
+
   return (
     <div className="App">
       <h1> Hey Everyone!</h1>
+      <input type =  "text" value = {userName} onChange={(e) => setUserName(e.target.value)}/>
+      <button>Send</button>
     </div>
   );
 }
